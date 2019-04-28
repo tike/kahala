cc1d39e27c129a500c5dd92bcce86b1131cc40d4
tike <timflex@gmx.de>
2019-04-28 12:43:03 +0200

    wire up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PlayersComponent } from './players/players.component';
 import { HelpComponent } from './help/help.component';
 
 const routes: Routes = [
+  { path: 'players', component: PlayersComponent },
   { path: 'help', component: HelpComponent },
   { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
