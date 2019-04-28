4f207a3c7cb0b225d7db544e587b72bfa3f42dd4
tike <timflex@gmx.de>
2019-04-28 12:41:36 +0200

    add players component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelpComponent } from './help/help.component';
+import { PlayersComponent } from './players/players.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelpComponent
+    HelpComponent,
+    PlayersComponent
   ],
   imports: [
     BrowserModule,
