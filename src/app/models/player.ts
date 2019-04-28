2ecb8342381124f3fc4038ccec5420bd96d0c987
tike <timflex@gmx.de>
2019-04-28 14:22:58 +0200

    spell out player component functionality

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -3,4 +3,11 @@ export class Player {
   name: string;
   gamesPlayed: number;
   Wins: number;
+
+  constructor(name: string, id?: number) {
+    this.id = id !== undefined ? id: 0;
+    this.name = name;
+    this.gamesPlayed = 0;
+    this.Wins = 0;
+  }
 }
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Player } from '../models/player';
+import { PlayerService } from '../services/player.service';
+
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
   styleUrls: ['./players.component.scss']
 })
 export class PlayersComponent implements OnInit {
+  private players: Player[];
 
-  constructor() { }
+  constructor(private playerSvc: PlayerService) {}
 
   ngOnInit() {
+    this.getPlayers();
+  }
+
+  getPlayers(): void {
+    this.playerSvc.getPlayers()
+      .subscribe(players => {
+        this.players = players
+      });
+  }
+
+  getHighscores(): Player[] {
+    if (this.players === undefined) {
+      this.getPlayers();
+    }
+    return this.players.sort((p1, p2) => {
+      return p1.Wins < p2.Wins ? 1 : -1;
+    });
+  }
+
+
+  addPlayer(name: string) {
+    name = name.trim();
+    if (!name) { return; }
+    this.playerSvc.addPlayer(new Player(name))
+      .subscribe(player => {
+        this.players.push(player);
+      });
   }
 
 }
