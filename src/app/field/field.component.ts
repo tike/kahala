f265ff46d07e798b947ba053078792b4953d6d88
tike <timflex@gmx.de>
2019-04-28 18:06:24 +0200

    basic gameplay works

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { Field } from '../models/field';
 
+import { GameService } from '../services/game.service';
+
 @Component({
   selector: 'app-field',
   templateUrl: './field.component.html',
@@ -11,9 +13,26 @@ export class FieldComponent implements OnInit {
   @Input() player: number;
   @Input() field: Field;
 
-  constructor() { }
+  showLeft: boolean;
+  showRight: boolean;
 
-  ngOnInit() {
+  constructor(private gameSvc: GameService) {}
+
+  ngOnInit() {}
+
+  toggleArrows() {
+    this.showLeft = !this.showLeft;
+    this.showRight = !this.showRight;
   }
 
+  canStart() {
+    if (this.field.canPlay()) {
+      this.toggleArrows();
+    }
+  }
+
+  startMove(direction: boolean) {
+    this.gameSvc.startMove(this.player, this.field.getID(), direction);
+    this.toggleArrows();
+  }
 }
